e8d5d3b98e93b9d0b994a0d5cb71654f68569883
Vandini <egor96471@gmail.com>
2019-12-02 17:40:18 +0200

    input complete

--- src/main/java/Example1.java
+++ src/main/java/Example1.java
@@ -1,2 +1,12 @@
-public class Exemple1 {
+public class Example1 {
+    public static void main(String[] args) {
+        Example1Method method = new Example1Method();
+        String name = method.inputName();
+        String town = method.inputTown();
+        int age = method.inputAge();
+        String hobby = method.inputHobby();
+
+
+
+    }
 }
--- src/main/java/Example1Method.java
+++ src/main/java/Example1Method.java
@@ -1,2 +1,37 @@
-public class Example1Method {
+import java.util.Scanner;
+
+class Example1Method {
+private Scanner scanner = new Scanner(System.in);
+
+    String inputName(){
+        System.out.print("Введіть ваше ім'я - ");
+        return scanner.next();
+    }
+
+    String inputTown(){
+        System.out.print("Введіть ваше місто - ");
+        return scanner.next();
+    }
+
+    int inputAge(){
+        System.out.print("Введіь ваш вік - ");
+        test();
+        return scanner.nextInt();
+    }
+
+    String inputHobby(){
+        System.out.print("Введіть ваше хобі - ");
+        return scanner.next();
+    }
+
+    private void test() {
+        scanner.nextLine();
+        while (!scanner.hasNextInt()) {
+            System.out.print("Ви ввели неправильне значення, будьласка введіть число:");
+            scanner.nextLine();
+        }
+    }
+
 }
+
+
